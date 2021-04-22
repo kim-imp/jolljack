d2c897d0c25464ef30c440fe54aac7eead687b83
kim-imp <kyj970803@naver.com>
2021-04-22 16:58:52 +0900

    2021.04.22 kimp

---
+++ joljack/Assets/Script/CreatureMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureMovement : MonoBehaviour
+{
+    public float movePower = 1f;
+
+    Animator animator;
+    Vector3 movement;
+    int movementFlag = 0;
+    int slimeHP = 30;
+    int slimeDMG = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponentInChildren<Animator>();
+        
+        StartCoroutine("ChangeMovement");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+         
+    }
+
+    //코루틴
+    IEnumerator ChangeMovement()
+    {
+        movementFlag = Random.Range(0, 3);
+
+        if (movementFlag == 0)
+            animator.SetBool("isMoving", false);
+
+        else
+            animator.SetBool("isMoving", true);
+
+        yield return new WaitForSeconds(3f);
+
+        StartCoroutine("ChangMovement");
+        Debug.Log("its : " + movementFlag );
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if(movementFlag == 1)
+        {
+            moveVelocity = Vector3.left;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if(movementFlag == 2)
+        {
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+
+}
