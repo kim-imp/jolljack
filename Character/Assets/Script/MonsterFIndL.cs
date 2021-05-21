169069853d3e212f47f60ea360eeb5ecae8e6784
passerby10 <66811453+passerby10@users.noreply.github.com>
2021-05-22 02:38:25 +0900

    210521
    Start RangeMonster But Do Nothing

---
+++ Character/Assets/Script/MonsterFIndL.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterFIndL : MonoBehaviour
+{
+    public bool isPlayerFind = false;
+     GameObject Slime;
+
+    void Awake()
+    {
+        Slime = GameObject.Find(this.transform.parent.name.ToString());
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            isPlayerFind = true;
+            Vector3 playerPos = collision.transform.position;
+            if(GetComponent<MonsterFInd>().isPlayerFind == true)
+            {
+                if (playerPos.x > transform.position.x)
+                {
+                    Slime.GetComponent<MonsterMove>().RushRight();
+                    //GetComponent<MonsterMove>().RushRight();
+                }
+                if (playerPos.x < transform.position.x)
+                {
+                    print(this.transform.parent.name.ToString());
+                    Slime.GetComponent<MonsterMove>().RushLeft();
+                    //GetComponent<MonsterMove>().RushLeft();
+                }
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            isPlayerFind = false;
+        }
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if(collision.tag == "Player")
+    //    {
+    //        isPlayerFind = true;
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if(collision.tag == "Player")
+    //    {
+    //        isPlayerFind = false;
+    //    }
+    //}
+}
